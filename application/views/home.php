92164738bb9788c5271fa8caca0c53582ba37c2d
rpariona81 <ronald.pariona.isidro@gmail.com>
2023-10-05 23:57:02 -0500

    add lib to homepage

--- application/views/home.php
+++ application/views/home.php
@@ -27,6 +27,13 @@
     <!-- Bootstrap css -->
     <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
     <link href="<?= base_url('public/app/lib/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <!-- Libraries CSS Files -->
+    <link href="<?= base_url('public/app/lib/owlcarousel/assets/owl.carousel.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('public/app/lib/owlcarousel/assets/owl.theme.default.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('public/app/lib/font-awesome/css/font-awesome.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('public/app/lib/animate/animate.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('public/app/lib/modal-video/css/modal-video.min.css') ?>" rel="stylesheet">
+
     <link href="<?= base_url('public/app/font-awesome/4.7.0/css/font-awesome.min.css') ?>" rel="stylesheet">
     <!-- Main Stylesheet File -->
     <!--<link href="<?= base_url('public/app/css/style.css') ?>" rel="stylesheet">-->
